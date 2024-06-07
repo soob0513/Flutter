cbe9d1be6f0184eb94ad5a20e65c145f1a22ec32
최수빈 <165890322+soob0513@users.noreply.github.com>
2024-06-07 13:30:05 +0900

    0607

--- flutter0605/lib/ex04_SizedBox.dart
+++ flutter0605/lib/ex04_SizedBox.dart
@@ -5,28 +5,46 @@ class ExSizedBox extends StatelessWidget {
 
   // sizedbox의 목적!
   // 1. child 위젯의 크기를 조정하기 위한 목적!
-  // 2. 위젯들 사이의 공간을 추가하기 위한 목적!
+  // 2. 위젯들 사이의 공간을 추가하기 위한 목적! ***
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          height: 200,
-          child: Container(
-            color: Colors.pinkAccent[100],
-            child: Row(
-              children: [
-                ElevatedButton(onPressed: (){}, child: Text('button1')),
-                ElevatedButton(onPressed: (){}, child: Text('button2')),
-              ],
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              color: Colors.blue,
+              child: Row(
+                children: [
+                  ElevatedButton(onPressed: (){}, child: Text('button1')),
+                  ElevatedButton(onPressed: (){}, child: Text('button2')),
+                ],
+              ),
             ),
-          ),
+            Container(
+              child: Row(
+                children: [
+                  Text('첫번째 텍스트'),
+
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Container(
+                      // color: Colors.redAccent,
+                    ),
+                  ),
+
+                  Text('두번째 텍스트'),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-
     );
   }
 }
